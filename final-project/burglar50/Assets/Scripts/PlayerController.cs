cd40c414c67df2520c5f56e4d40a4c73c5e1f50a
MasterUri <deathwarks@yahoo.com>
2024-05-08 12:40:00 -0300

    burglar50 start

---
+++ final-project/burglar50/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool facingRight = true;
+    public bool bag = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
